e6056a690eefe99dcfa579c6f3b432a3451cbc63
MChandraR <2201020103@student.umrah.ac.id>
2025-02-14 23:14:09 +0700

    Optimize grid layout on anggota page

--- src/app/anggota/page.tsx
+++ src/app/anggota/page.tsx
@@ -4,6 +4,9 @@ import member from "@/data/people";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import { FaCaretDown } from "react-icons/fa";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ['latin'] })
 
 export default function Page(){
     const [isOpen, setIsOpen] = useState(false);
@@ -33,7 +36,7 @@ export default function Page(){
 
     return (
         <MainLayout>
-            <div className="min-h-[100vh] p-6 md:p-16 pt-32 md:pt-32 flex flex-col gap-4">
+            <div className="min-h-[100vh] p-6 mb-20 md:p-16 pt-32 md:pt-32 flex flex-col gap-4">
                 <div className="flex gap-4 h-12 item-center" >
                     <h1 className="text-black font-bold">Periode : </h1>
                     <div className="flex justify-center min-h-screen z-10">
@@ -76,13 +79,13 @@ export default function Page(){
                         </div>
                     </div>
                 </div>
-                <h1 className="w-full md:w-1/2 uppercase font-bold text-4xl text-darkerBlue">Meet Our Team and Expert In Robotika UMRAH</h1>
-                <p className="w-full md:-1/2 text-gray-700 font-bold">Perkenalkan para anggota kami yang selalu berdedikasi dan terus melakukan riset  demi menghasilkan penemuan baru. </p>
-                <div className="grid grid-cols-2 md:grid-cols-6 gap-6 mt-8">
+                <h1 className={`${inter.className}w-full md:w-2/3 uppercase font-extrabold text-5xl text-darkerBlue`}>Meet Our Team and Expert In Robotika UMRAH</h1>
+                <p className={`${inter.className}w-full md:-1/2 text-gray-700 font-extrabold`}>Perkenalkan para anggota kami yang selalu berdedikasi dan terus melakukan riset  demi menghasilkan penemuan baru. </p>
+                <div className="grid grid-cols-2 md:grid-cols-4 gap-6 mt-8">
                     {member.map((item,key)=>(
-                        <div key={key}>
+                        <div className="flex flex-col " key={key}>
                             <Image 
-                            className="rounded-full object-cover object-top grayscale w-[calc(48vw_-_2rem)] h-[calc(48vw_-_2rem)] md:w-[calc(16vw_-_2rem)] md:h-[calc(16vw_-_2rem)] bg-gray-100"
+                            className="self-center rounded-full object-cover object-top grayscale w-[calc(48vw_-_2rem)] h-[calc(48vw_-_2rem)] md:w-[calc(18vw_-_2rem)] md:h-[calc(18vw_-_2rem)] bg-gray-100"
                             src={`/images/person/${item.id}.png`} width={480} height={480} alt="member-photo"/>
                             <h1 className="font-bold text-lg md:text-xl text-primaryYellow text-center">{item.name}</h1>
                             <h1 className="font-bold text-sm md:text-sm text-primaryYellow text-center">{item.role}</h1>
