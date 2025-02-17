c0cff9dcb7d230d64853fba55b2f6c4995da05c0
MChandraR <2201020103@student.umrah.ac.id>
2025-02-18 01:22:05 +0700

    Fix some eslint rules viloation

--- src/app/admin/post/[id]/page.tsx
+++ src/app/admin/post/[id]/page.tsx
@@ -36,7 +36,7 @@ export default function Page(){
                 setUrl(data.url??"");
             }
         });
-    },[]);
+    },[param.id]);
 
     const handleSubmit = ()=>{
         console.log("Proses");
